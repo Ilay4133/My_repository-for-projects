d312a706af85a6692f532d7da437423ca49b4fb1
Ilay_(Cyn) <148774440+Ilay4133@users.noreply.github.com>
2024-03-28 21:00:29 +0700

    Add files via upload

--- Videoplayer website/ new user registration window/controller.py
+++ Videoplayer website/ new user registration window/controller.py
@@ -1,36 +1,34 @@
 import sys
 from model import entry_user, registration_new_user
-from view import get_user_nickname, get_user_name, get_user_second_name, get_user_city,get_user_age, get_user_password, get_check_user_nickname,get_chack_user_password, start_registrat, start_ent, start_clos
+from view import geting_user_nickname, geting_user_name, geting_user_second_name, geting_user_city,geting_user_age, geting_user_password, geting_check_user_nickname,geting_chack_user_password, start_registrating, start_enting, start_closing
 
-from view import main().__import__(get_user_nickname)
-
-def start_code():
-    while True:
-        if start_registrat() == 1:
-            start_registration()
-            break
-        elif start_ent() == 1:
-            start_entry()
-            break
-        elif start_clos() == 1:
-            start_close()
-            break
 def start_registration():
-    user_nickname = get_user_nickname()
-    user_name = get_user_name()
-    user_second_name = get_user_second_name()
-    user_city = get_user_city()
-    user_age = get_user_age()
-    user_password = get_user_password()
+    user_nickname = geting_user_nickname
+    user_name = geting_user_name
+    user_second_name = geting_user_second_name
+    user_city = geting_user_city
+    user_age = geting_user_age
+    user_password = geting_user_password
     registration_new_user(user_nickname, user_name, user_second_name, user_city, user_age, user_password)
 
-
 def start_entry():
-    check_user_nickname =  get_check_user_nickname()
-    chack_user_password = get_chack_user_password()
+    check_user_nickname =  geting_check_user_nickname()
+    chack_user_password = geting_chack_user_password()
     entry_user(check_user_nickname, chack_user_password)
-
 
 def start_close():
     sys.exit(404)
+
+
+def start_code():
+    while True:
+        if start_registrating == 1:
+            start_registration()
+            break
+        elif start_enting() == 1:
+            start_entry()
+            break
+        elif start_closing() == 1:
+            start_close()
+            break
 
