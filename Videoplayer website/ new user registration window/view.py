6aed960b8f4d204fe0396f2ecca700790b219f66
Ilay_(Cyn) <148774440+Ilay4133@users.noreply.github.com>
2024-03-28 21:02:25 +0700

    Add files via upload

--- Videoplayer website/ new user registration window/view.py
+++ Videoplayer website/ new user registration window/view.py
@@ -1,41 +1,77 @@
 import flet as ft
 import sys
 
+geting_user_nickname=""
+geting_user_name=""
+geting_user_second_name=""
+geting_user_city=""
+geting_user_age=""
+geting_user_password=""
+geting_check_user_nickname=""
+geting_chack_user_password=""
+start_registrating=0
+start_enting=0
+start_closing=0
+
 def main(page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+
+
     def get_user_nickname():
-        return txt_nickname.label
+        global geting_user_nickname
+        geting_user_nickname=txt_nickname.label
+        return geting_user_nickname
 
     def get_user_name():
+        global geting_user_name
+        geting_user_name=txt_name.label
         return txt_name.label
 
     def get_user_second_name():
+        global geting_user_second_name
+        geting_user_second_name=
         return txt_second_name.label
 
     def get_user_city():
+        global geting_user_city
+        geting_user_city=
         return txt_city.label
 
     def get_user_age():
+        global geting_user_age
+        geting_user_age=
         return txt_age.label
 
     def get_user_password():
+        global geting_user_password
+        geting_user_password=
         return txt_pasword.label
 
     def get_check_user_nickname():
+        global geting_user_name
+        geting_user_name=
         return txt_check_nickname.label
 
     def get_chack_user_password():
+        global geting_user_name
+        geting_user_name=
         return txt_check_pasword.label
 
     def start_registrat():
-        return 1
+        global start_registrating
+        start_registrating=1
+        return start_registrating
 
     def start_ent():
+        global start_enting
+        start_enting = 1
         return 1
 
     def start_clos():
+        global start_start_closing
+        start_start_closing = 1
         return 1
 
     def button_clicked_enter1(e):
@@ -114,6 +150,8 @@ def main(page):
         close.visible = True
         hom.visible = False
 
+
+
     txt_nickname = ft.TextField(value="", hint_text="Enter nickname", width=300)
     txt_name = ft.TextField(value="", hint_text="Enter your name", width=300)
     txt_second_name = ft.TextField(value="", hint_text="Enter your second name", width=300)
@@ -123,15 +161,14 @@ def main(page):
     txt_check_nickname = ft.TextField(value="", hint_text="Enter nickname", width=300)
     txt_check_pasword = ft.TextField(value="", hint_text="Enter password", width=300)
 
-    regist=ft.ElevatedButton(text="Registration",width=300, on_click=button_clicked_start_registration)
-    login=ft.ElevatedButton(text="Login",width=300, on_click=button_clicked_start_entry)
-    close=ft.ElevatedButton(text="Close",width=300, on_click=button_clicked_start_close)
-    hom=ft.ElevatedButton(text="Back", on_click=home)
-    enter1=ft.ElevatedButton(text="Enter", on_click=button_clicked_enter1)
-    enter2=ft.ElevatedButton(text="Enter", on_click=button_clicked_enter2)
-    i = 1
-    if i == 1:
-        del1()
+    regist=ft.ElevatedButton(text="Registration",width=300, height= 40, on_click=button_clicked_start_registration)
+    login=ft.ElevatedButton(text="Login",width=300, height= 40, on_click=button_clicked_start_entry)
+    close=ft.ElevatedButton(text="Close",width=300, height= 40, color="red400", on_click=button_clicked_start_close)
+    hom=ft.ElevatedButton(text="Back",width=300, on_click=home)
+    enter1=ft.ElevatedButton(text="Enter",width=300, on_click=button_clicked_enter1)
+    enter2=ft.ElevatedButton(text="Enter",width=300, on_click=button_clicked_enter2)
+
+    del1()
 
     page.add(
         ft.Column(
